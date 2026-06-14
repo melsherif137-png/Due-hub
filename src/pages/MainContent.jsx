66fef6ef206ec64b1c9e434ba866e26f67923e2d
ziad atia <zezoatia10@gmail.com>
2026-06-14 17:05:28 +0300

    Update project

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "./AppLayout";
 import DashboardLayout from "./DashboardLayout";
+import Login from "./Registration/Login";
 
 const MainContent = () => {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />} />
       <Route path="/dashboard" element={<DashboardLayout />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
